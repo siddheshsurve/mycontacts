b298f788ba121364594d9e9afc043f10bc9a1ecf
Siddhesh Surve <sysurve24@gmail.com>
2023-07-09 14:17:29 +0000

    tried thunder client with simple route

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
-const dotenv = require("dotenv").config()
+const dotenv = require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res) => {
+    res.status(200).json({message : "Get all contacts"});
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
