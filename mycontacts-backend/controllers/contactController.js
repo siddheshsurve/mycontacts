5a34c9b2eb452623971263fc346adb42ed23dda7
Siddhesh Surve <sysurve24@gmail.com>
2023-07-10 12:14:08 +0000

    er

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,10 +1,10 @@
-//@desc Get all contacts
-//@route Get /api/contact
-//@access public
-const getContact = (req, res) => {
-    res.status(200).json({ message : "Get all contacts" });
-};
+// //@desc Get all contacts
+// //@route Get /api/contact
+// //@access public
+// const getContact = (req, res) => {
+//     res.status(200).json({ message : "Get all contacts" });
+// };
 
 
 
-module.exports = { getContact };
+// module.exports = { getContact };
